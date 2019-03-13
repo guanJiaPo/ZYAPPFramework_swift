95361907dc09eed66e42e79c15bac021c315da72
shizhiyuan <shizhiyuan369@163.com>
2019-03-13 18:09:03 +0800

    添加UIButton扩展方法

--- ZYAPPFramework/ZYAPPFramework/Category/ZYCategory/UIButton+ZYCategory.swift
+++ ZYAPPFramework/ZYAPPFramework/Category/ZYCategory/UIButton+ZYCategory.swift
@@ -8,10 +8,48 @@
 
 import UIKit
 
+enum ZYButtonImagePosition: Int {
+    /// 左图右字
+    case left
+    /// 右图左字
+    case right
+    /// 上图下字
+    case top
+    /// 下图上字
+    case bottom
+}
+
 extension UIButton {
     
     func setBackgroundColor(color: UIColor, forState:UIControl.State) {
         self.setBackgroundImage(UIImage.imageWithColor(color: color), for: forState)
+    }
+    
+
+    /// 调整图片方向
+    ///
+    /// - Parameters:
+    ///   - position: 图片位置
+    ///   - space: 文字与图片的间距
+    func adjustImagePosition(position: ZYButtonImagePosition, space: CGFloat) {
+        let imageWidth = self.imageView?.intrinsicContentSize.width ?? 0;
+        let imageHeight = self.imageView?.intrinsicContentSize.height ?? 0;
+        let labelWidth = self.titleLabel?.intrinsicContentSize.width ?? 0;
+        let labelHeight = self.titleLabel?.intrinsicContentSize.height ?? 0;
+        switch position {
+        case ZYButtonImagePosition.left:
+            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: -space / 2, bottom: 0, right: space / 2)
+            self.titleEdgeInsets = UIEdgeInsets(top: 0, left: space / 2, bottom: 0, right: -space / 2)
+        case ZYButtonImagePosition.right:
+            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: labelWidth + space / 2, bottom: 0, right: -labelWidth - space/2)
+            self.titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageWidth - space / 2, bottom: 0, right: imageWidth + space / 2)
+        case ZYButtonImagePosition.top:
+            self.imageEdgeInsets = UIEdgeInsets(top: -labelHeight - space / 2, left: 0, bottom: 0, right: -labelWidth)
+            self.titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: -imageHeight - space / 2, right: 0)
+        case ZYButtonImagePosition.bottom:
+            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: -labelHeight - space / 2, right: -labelWidth)
+            self.titleEdgeInsets = UIEdgeInsets(top: -imageHeight - space / 2, left: -imageWidth, bottom: 0, right: 0)
+        }
     }
 }
 
